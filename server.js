d6f4b2cf26bcbf8e32bc09f4070d42303628432e
Mark Miller <markmiller7858@gmail.com>
2022-02-04 21:48:39 -0500

    templates started

--- server.js
+++ server.js
@@ -2,10 +2,30 @@ const express = require('express');
 const routes = require('./Controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess={
+    secret:process.env.DB_SECRET,
+    cookie:{},
+    resave:false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db:sequelize
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
