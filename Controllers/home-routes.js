f4fb53b302c78ebc1c04be167650cb25ec17ab15
Mark Miller <markmiller7858@gmail.com>
2022-02-10 13:11:46 -0500

    helper added, partials started

--- Controllers/home-routes.js
+++ Controllers/home-routes.js
@@ -1,7 +1,44 @@
+const { Post, User, Comment } = require('../models');
+
 const router = require('express').Router();
 
 router.get('/', (req, res)=>{
-    res.render('homepage');
+    console.log(req.session);
+    Post.findAll({
+        attributes:['id',
+                    'title',
+                    'post_content',
+                    'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes:['id', 
+                            'comment_text',
+                            'post_id',
+                            'user_id',
+                            'created_at' ],
+                include:{
+                    model: User,
+                    attributes:['username']
+                }
+            },
+            {
+                model: User,
+                attributes:['username']
+            }
+        ]
+    })
+    .then(dbPostData =>{
+        const posts = dbPostData.map(post=>post.get({plain:true}));
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json(err);
+    });  
 });
 
 module.exports = router;
